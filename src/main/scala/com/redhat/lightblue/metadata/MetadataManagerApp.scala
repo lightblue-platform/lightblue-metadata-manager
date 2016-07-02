700e9651a2af5aff6e149bd85a547db0aad2f0d7
Marek Paterczyk <mpatercz@redhat.com>
2016-07-02 16:44:05 -0400

    Env vars

--- src/main/scala/com/redhat/lightblue/metadata/MetadataManagerApp.scala
+++ src/main/scala/com/redhat/lightblue/metadata/MetadataManagerApp.scala
@@ -17,54 +17,87 @@ object MetadataManagerApp extends App {
     val logger = LoggerFactory.getLogger(MetadataManagerApp.getClass);
 
     val options = new Options();
+    
+    try {
+    
 
-    val lbClientOption = Option.builder("lc")
-        .required(true)
-        .desc("Configuration file for lightblue-client")
-        .longOpt("lightblue-client")
-        .hasArg()
-        .argName("lightblue-client.properties")
-        .build();
+    
+        val lbClientOption = Option.builder("lc")
+            .desc("Configuration file for lightblue-client")
+            .longOpt("lightblue-client")
+            .hasArg()
+            .argName("lightblue-client.properties")
+            .build();
 
-    val helpOption = Option.builder("h")
-        .required(false)
-        .desc("prints usage")
-        .longOpt("help")
-        .build();
+        val envOption = Option.builder("e")
+            .desc("Lightblue environment (export LB_CLIENT_{ENV}=/home/user/lightblue-clients/lightblue-client-{ENV}.properties")
+            .longOpt("environment")
+            .hasArg()
+            .argName("environment, e.g. dev")
+            .build();
 
-    options.addOption(lbClientOption);
-    options.addOption(helpOption);
+        val helpOption = Option.builder("h")
+            .required(false)
+            .desc("prints usage")
+            .longOpt("help")
+            .build();
 
-    val parser = new DefaultParser();
-    val cmd = parser.parse(options, args);
+        options.addOption(lbClientOption);
+        options.addOption(envOption);
+        options.addOption(helpOption);
 
-    if (cmd.hasOption('h')) {
-        // automatically generate the help statement
+        val parser = new DefaultParser();
+        val cmd = parser.parse(options, args);
+
+        if (cmd.hasOption('h')) {
+            // automatically generate the help statement
+            printUsage(options)
+            System.exit(0);
+        }
+
+        if (cmd.hasOption("lc") && cmd.hasOption("e") || !cmd.hasOption("lc") && !cmd.hasOption("e")) {
+            throw new ParseException("Either -lc or -e is required");
+        }
+
+        val lbClientFilePath = if (cmd.hasOption("lc")) cmd.getOptionValue("lc") else {
+
+            val envVarName = "LB_CLIENT_"+cmd.getOptionValue("e").toUpperCase()
+            System.getenv(envVarName) match {
+                case null => throw new ParseException(s"""${envVarName} is not set!""")
+                case x => x
+            }
+        }
+        logger.debug(s"""Reading lightblue client configuration from ${lbClientFilePath}""")
+
+        val client = new LightblueHttpClient(lbClientFilePath);
+
+        val manager = new MetadataManager(client)
+
+    //    val defaultUserVersion = manager.getEntityVersion("user", entityVersionDefault) match {
+    //        case Some(x) => x
+    //        case None    => throw new Exception("Version not found!")
+    //    }
+    //
+    //    val userEntity = manager.getEntity("user", defaultUserVersion)
+
+        manager.getEntities("^user.*".r, entityVersionDefault) foreach { entity =>
+            val fileName = s"""${entity.name}.json"""
+            logger.info(s"""Saving $fileName...""")
+            Files.write(Paths.get(s"""${entity.name}.json"""), entity.text.getBytes)
+        }
+
+    } catch {
+        case pe: ParseException => {
+            logger.error(pe.getMessage)
+            printUsage(options)
+            System.exit(0);
+        }
+    }
+
+    def printUsage(options: Options) {
         val formatter = new HelpFormatter();
-        formatter.printHelp(MetadataManagerApp.getClass.getSimpleName(), options);
-        System.exit(0);
+        formatter.printHelp(120, MetadataManagerApp.getClass.getSimpleName, "", options, null);
     }
-
-    val lbClientFilePath = cmd.getOptionValue("lc");
-
-    val client = new LightblueHttpClient(lbClientFilePath);
-
-    val manager = new MetadataManager(client)
-
-//    val defaultUserVersion = manager.getEntityVersion("user", entityVersionDefault) match {
-//        case Some(x) => x
-//        case None    => throw new Exception("Version not found!")
-//    }
-//
-//    val userEntity = manager.getEntity("user", defaultUserVersion)   
-    
-    manager.getEntities("^user.*".r, entityVersionDefault) foreach { entity =>
-        val fileName = s"""${entity.name}.json"""
-        logger.info(s"""Saving $fileName...""") 
-        Files.write(Paths.get(s"""${entity.name}.json"""), entity.text.getBytes)
-    }
-    
-    
 
     
 }
